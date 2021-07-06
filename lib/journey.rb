175e716295932fc7be609ccb36f7f4f0e313b099
karimabuseer <karimabuseer@gmail.com>
2021-07-06 20:31:13 +0100

    Fixed tests to work with journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,7 +2,7 @@ class Journey
   attr_reader :entry_station, :exit_station
   MINIMUM_FARE = 1
   PENALTY_FARE = 6
-  def initialize(entry_station)
+  def initialize(entry_station = nil)
     @entry_station = entry_station
     @exit_station
     @fare = 1
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,1 +1,15 @@
 require 'journey'
+
+describe Journey do
+  let (:entry_station) {double :entry_station}
+  let (:exit_station) {double :exit_station }
+  it 'Will store journey on touch out' do
+    journey1 = Journey.new(entry_station)
+    journey1.leave_station(exit_station)
+    expect(journey1.log).to include(entry_station => exit_station)
+  end
+
+  it 'Default fare is penalty value' do
+    expect(subject.fare).to eq(6)
+  end
+end
